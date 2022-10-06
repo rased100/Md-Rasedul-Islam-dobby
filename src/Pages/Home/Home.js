fa6b782a3039bcc828afef889b465a63458e4c4b
Md Rasedul Islam <86698952+rased100@users.noreply.github.com>
2022-10-06 23:58:23 +0600

    hame page

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>This is Home Page</h1>
+            <h2>Dobby Ads</h2>
+        </div>
+    );
+};
+
+export default Home;
