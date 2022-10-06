2cb37c7c3d340f08ec1a16dc32e490223412e824
Md Rasedul Islam <86698952+rased100@users.noreply.github.com>
2022-10-07 04:08:15 +0600

    firebase init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import * as React from "react";
 import './App.css';
 import Home from './Pages/Home/Home';
+import { Routes, Route, Link } from "react-router-dom";
 import Navbar from './Pages/Shared/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
-      <Home></Home>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -1,30 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/images/DobbyLogo_s.webp';
 
 const Navbar = () => {
     return (
-        <div class="navbar bg-base-100">
-            <div class="navbar-start">
-                <div class="dropdown">
-                    <label tabindex="0" class="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex="0" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Item 1</a></li>
-                        <li><a>Item 2</a></li>
-                        <li><a>Item 3</a></li>
+                    <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><Link>Home</Link></li>
+                        <li><Link>About</Link></li>
+                        <li><Link>Contact</Link></li>
                     </ul>
                 </div>
-                <a class="btn btn-ghost normal-case text-xl">Dobby Ads</a>
+                <a className="btn btn-ghost normal-case text-xl bg-primary mx-2"><img src={logo} alt="dobby" /></a>
             </div>
-            <div class="navbar-center hidden lg:flex">
-                <ul class="menu menu-horizontal p-0">
-                    <li><a>Item 1</a></li>
-                    <li><a>Item 2</a></li>
-                    <li><a>Item 3</a></li>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal p-0">
+                    <li><Link>Home</Link></li>
+                    <li><Link>About</Link></li>
+                    <li><Link>Contact</Link></li>
                 </ul>
             </div>
-            <div class="navbar-end">
-                <a class="btn">Get started</a>
+            <div className="navbar-end">
+                <Link className="btn mx-2">Upload Image</Link>
+                <a className="btn mx-2">Login</a>
+                <a className="btn mx-2">Signup</a>
             </div>
         </div>
     );
