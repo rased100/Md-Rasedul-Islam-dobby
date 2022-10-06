103e11562248801823fbdedbe90fe7df507179b8
Md Rasedul Islam <86698952+rased100@users.noreply.github.com>
2022-10-07 00:30:51 +0600

    tailwind and daisyUI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import Home from './Pages/Home/Home';
+import Navbar from './Pages/Shared/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <Home></Home>
     </div>
   );
