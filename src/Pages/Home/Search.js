703ea15d70fc1e95da59b5a71378ec27965e6128
Md Rasedul Islam <86698952+rased100@users.noreply.github.com>
2022-10-08 22:02:33 +0600

    finish

--- src/Pages/Home/Search.js
+++ src/Pages/Home/Search.js
@@ -25,13 +25,13 @@ const Search = () => {
 
             <div className='py-10 flex justify-center'>
                 <div className='grid grid-cols-1'>
-                    <div class="form-control p-10 mx-auto">
+                    <div className="form-control p-10 mx-auto">
                         <form onSubmit={handleSearch}>
-                            <div class="form-control">
-                                <div class="input-group">
-                                    <input type="text" name="name" placeholder="Search…" class="input input-bordered" />
-                                    <button type='submit' class="btn btn-square">
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                            <div className="form-control">
+                                <div className="input-group">
+                                    <input type="text" name="name" placeholder="Search…" className="input input-bordered" />
+                                    <button type='submit' className="btn btn-square">
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
                                     </button>
                                 </div>
                             </div>
